3c51f386408c6a8e9fb957077f8ff9074e1820c4
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2019-12-27 12:23:46 -0500

    config: add placeholder setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.0',
+    description='Repository of original statistical analysis in Stata and then replicated using both Python and R',
+    author='Michael Sedelmeyer',
+    license='MIT',
+)
